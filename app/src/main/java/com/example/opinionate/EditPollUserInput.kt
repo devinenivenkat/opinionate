2f6277789189ba1a4f7408a564fbbdaaf1c9e622
dsvrc <101639707+dsvrc@users.noreply.github.com>
2023-05-25 15:47:29 +0530

    edit polls feature added

---
+++ app/src/main/java/com/example/opinionate/EditPollUserInput.kt
@@ -0,0 +1,11 @@
+package com.example.letmeknow
+
+import android.net.Uri
+
+class EditPollUserInput(
+    var viewType:String,
+    var viewData: String,
+    var viewUrl: String,
+    var newviewUri: Uri,
+    var index: Int
+)
